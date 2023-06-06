b79fee460d6982ddd1a45fe440a1715b5cffaa6d
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-06-06 14:48:20 +0530

    making user and notes Schema And Routes of auth & notes

--- backend/index.js
+++ backend/index.js
@@ -1,2 +1,20 @@
 const connectToMongo = require('./db');
+const express = require("express");
+
 connectToMongo();
+
+const app = express();
+const port = 3000;
+
+app.use("/api/auth",require('./routes/auth'));
+app.use("/api/notes",require('./routes/notes'));
+app.get("/", (req, res) => {
+  res.send("Hello Rahul!");
+});
+app.get("/api", (req, res) => {
+  res.send("Hello login!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
