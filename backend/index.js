297a70111d058c41a0b1201f1a549fb0cea27b60
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-06-06 16:33:23 +0530

    create user and save data in database

--- backend/index.js
+++ backend/index.js
@@ -2,10 +2,10 @@ const connectToMongo = require('./db');
 const express = require("express");
 
 connectToMongo();
-
 const app = express();
-const port = 3000;
+const port = 5000;
 
+app.use(express.json());
 app.use("/api/auth",require('./routes/auth'));
 app.use("/api/notes",require('./routes/notes'));
 app.get("/", (req, res) => {
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,8 +1,38 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+const { body, validationResult } = require("express-validator");
 
-router.get('/', (req, res) => {
-    res.json([]);
+//create a new user "api/auth/createuser"
+router.post("/createuser", [
+    body("name",'Enter a valid name').isLength({ min: 3 }),
+    body("email",'Enter a valid email').isEmail(),
+    body("password",'Enter atleast 5 characters').isLength({ min: 5 }),
+], async (req, res) => {
+    // check for errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array()});
+    }
+    try {
+        let user = await User.findOne({ email: req.body.email });
+        //check if user already exists or not
+      if (user) {
+        return res
+          .status(400)
+          .json({ error: "a user with that email already exists" });
+      }
+      user = await User.create({
+        name: req.body.name,
+        password: req.body.password,
+        email: req.body.email,
+      });
+      res.json(user);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("Some error occurred");
+    };
+
 });
 
 module.exports = router;
