2f9e7c51666f7182cd66b41ba4e55b1d9a937c2d
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-06-08 14:49:01 +0530

    Set up react and Make Context Api

---
+++ src/context/notes/NoteState.js
@@ -0,0 +1,24 @@
+import NoteContext from './noteContext';
+import { useState } from 'react';
+const NoteState = (props) => {
+    const s1 = {
+        "name": "rahul",
+        "class":"5a",
+    }
+    const [state, setState] = useState(s1);
+    const update = () => {
+        setTimeout(() => {
+            setState({
+                "name": "sharma",
+                "class":"5b",
+            })
+        }, 5000);
+    }
+    return (
+        <NoteContext.Provider value={{state,update}}>
+            {props.children}
+        </NoteContext.Provider>
+    )
+};
+
+export default NoteState;
