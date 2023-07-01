21fe14e354828865e065955ca9a9bc815abad6db
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-07-01 15:24:14 +0530

    build

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,23 @@
 const connectToMongo = require("./db");
 const express = require("express");
 var cors = require("cors");
+const path = require("path");
 
 connectToMongo();
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/auth", require("./routes/auth"));
 app.use("/api/notes", require("./routes/notes"));
+
+app.use(express.static(path.join(__dirname, "../build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../build/index.html"));
+});
+
 app.get("/", (req, res) => {
   res.send("Hello Rahul!");
 });
