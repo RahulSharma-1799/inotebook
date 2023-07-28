367b43b0bdb9476ee17667c0bbc724c33cdd8cc6
RahulSharma-1799 <2020ucp1799@mnit.ac.in>
2023-07-28 19:38:56 +0530

    solving error on url at login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,7 +6,7 @@ const Login = (props) => {
     let navigate = useNavigate();
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch(`${url}/api/auth/login`, {
+    const response = await fetch(`https://inotebook-24wn.onrender.com/api/auth/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
